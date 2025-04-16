b773d24fe025e777b42f8bf9d25c4aeeb827adf0
Marko <markoperisic178@gmail.com>
2025-04-16 18:14:18 +0200

    change database to postgres

---
+++ prisma/migrations/20250416150403_init/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "refresh_token_expiration_date" TIMESTAMP(3),
+    "email_verified" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "words" (
+    "id" SERIAL NOT NULL,
+    "word" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL,
+    "user_notes" TEXT NOT NULL,
+    "generated_notes" TEXT NOT NULL,
+    "audio" TEXT NOT NULL,
+    "repetitions" INTEGER NOT NULL,
+    "days" INTEGER NOT NULL,
+    "review_date" TIMESTAMP(3) NOT NULL,
+    "ease_factor" DOUBLE PRECISION NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "language" TEXT NOT NULL,
+
+    CONSTRAINT "words_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "id" ON "users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "unique_email" ON "users"("email");
