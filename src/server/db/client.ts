38386477c553be21e40a2cf65c784c9cfdca79a4
Marko <markoperisic178@gmail.com>
2026-05-14 18:53:07 +0200

    refactor: update Prisma client initialization to use PrismaPg adapter

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,14 +1,18 @@
 import "server-only";
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+import { PrismaPg } from "@prisma/adapter-pg";
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const prisma = new PrismaClient({ adapter });
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-  });
+// const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+//   });
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
