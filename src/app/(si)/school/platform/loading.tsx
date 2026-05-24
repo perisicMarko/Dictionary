91a917da3f9677440fd6b1e391c4c0832c8ea3f6
Marko <markoperisic178@gmail.com>
2026-05-24 14:30:40 +0200

    chore: remove school platform components and related logic from production
    - Remove ui related to the school platform, but leave the backend because it has some subscription management functions that are needed

--- src/app/(si)/school/platform/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex justify-center sm:items-center mt-40 sm:mt-0 w-full h-screen">
-      <div className="w-12.5 h-12.5 md:w-25 md:h-25 xl:w-37.5 xl:h-37.5 bg-main rounded-2xl morph-loader"></div>
-    </div>
-  );
-}
